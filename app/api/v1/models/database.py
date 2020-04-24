526a5af311d1694829bf8c146f799b7f6e5406fd
Arrotech <arrotechdesign@gmail.com>
2020-04-24 21:49:59 +0300

    enable the database file to be runnas a script

--- app/api/v1/models/database.py
+++ app/api/v1/models/database.py
@@ -74,3 +74,7 @@ class Database:
         self.conn.commit()
         self.curr.close()
         return fetch_one
+
+if __name__ == '__main__':
+    Database().destroy_table()
+    Database().create_table()
